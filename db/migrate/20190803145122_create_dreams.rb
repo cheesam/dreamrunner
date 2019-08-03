4db2b94918b38a29b439544a62ab9bee796ebd99
Egor Volchkov <volchkov.uk@gmail.com>
2019-08-03 16:36:20 +0100

    built dreams model and views for new and edit

---
+++ db/migrate/20190803145122_create_dreams.rb
@@ -0,0 +1,13 @@
+class CreateDreams < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dreams do |t|
+      t.string :name
+      t.string :category
+      t.float :price
+      t.string :description
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_140534) do
+ActiveRecord::Schema.define(version: 2019_08_03_145122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dreams", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.float "price"
+    t.string "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_dreams_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_08_03_140534) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dreams", "users"
 end
