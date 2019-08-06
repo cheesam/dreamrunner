de1a241ae2a326985b4c3a88987ae2343a1c6ab9
Sunny Thaicharoen <thaicharoens@me.com>
2019-08-06 20:44:20 +0100

    Added Cloudinary upload to User profile edit. Navbar image not tied to user yet.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :dreams, dependent: :destroy
+  mount_uploader :photo, PhotoUploader
 end
