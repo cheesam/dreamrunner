3bb50e94d66fe7d0d2c1e65bf08934d9b44fbada
Sunny Thaicharoen <thaicharoens@me.com>
2019-08-06 20:27:13 +0100

    Added Cloudinary and CarrierWave to Dreams model. Image upload working, linked to Sunny's Cloudinary.

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,3 +1,4 @@
 class Dream < ApplicationRecord
   belongs_to :user
+  mount_uploader :photo, PhotoUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_145122) do
+ActiveRecord::Schema.define(version: 2019_08_06_191502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_08_03_145122) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_dreams_on_user_id"
   end
 
