7b2832b742941169617f16bc0781432fad61e1ac
Sam Cheesman <s.cheesman1234500@gmail.com>
2019-08-15 20:57:43 +0100

    pimped the shit out of the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
     @dreams = Dream.first(3)
+    @dreams_critics_choice = Dream.last(3)
   end
 end
