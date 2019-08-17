39be3d61b0f85f317cdad724813b24ed32f61c47
Sunny Thaicharoen <thaicharoens@me.com>
2019-08-17 15:09:05 +0100

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,6 @@ class PagesController < ApplicationController
   def home
     @dreams = Dream.first(3)
     @dreams_critics_choice = Dream.all.sort_by {|dream| dream.reviews.map {|review| review.rating }.sum / dream.reviews.size }
-    @dreams_critics_choice = Dream.last(3)
     @dreams_latest = Dream.last(3)
   end
 end
