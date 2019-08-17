469960e021edc807e2a5ded533a1fec88db56752
David Evans <david.evans0306@gmail.com>
2019-08-17 15:05:19 +0100

    I have edited the home pge navbar, added top dreams by review rating and edited the banner

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
     @dreams = Dream.first(3)
-    @dreams_critics_choice = Dream.last(3)
+    @dreams_critics_choice = Dream.all.sort_by {|dream| dream.reviews.map {|review| review.rating }.sum / dream.reviews.size }
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :dream
+  validates :accuracy, inclusion: { in: 0..10 } #message: "Please give put a score from 0-10"
+  validates :design, inclusion: { in: 0..10 } #message: "Please give put a score from 0-10"
+  validates :value, inclusion: { in: 0..10 } #message: "Please give put a score from 0-10"
 
   def rating
     (value + design + accuracy) / 3
